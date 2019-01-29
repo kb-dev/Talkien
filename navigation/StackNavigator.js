a7311bfc7037f77e0a025673ec88a701fbb48b9f
AamuLumi <florian@kauder.fr>
2019-01-29 18:07:21 +0100

    fix(app): Fix background transition and update app

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Animated, BackHandler, Easing, Platform, SafeAreaView, StatusBar, TouchableOpacity, View } from 'react-native';
 import { Constants, LinearGradient } from 'expo';
-import { createStackNavigator, NavigationActions } from 'react-navigation';
+import { createAppContainer, createStackNavigator, NavigationActions } from 'react-navigation';
 // Views
 import Home from '../views/Home';
 import About from '../views/About';
@@ -43,7 +43,7 @@ const RootStack = createStackNavigator(
     },
     {
         initialRouteName: 'Home',
-        navigationOptions: () => {
+        defaultNavigationOptions: () => {
             return {
                 header: null,
                 gesturesEnabled: true,
@@ -76,6 +76,8 @@ const RootStack = createStackNavigator(
     }
 );
 
+const AppContainer = createAppContainer(RootStack);
+
 function getActiveRouteName(navigationState) {
     if (!navigationState) {
         return null;
@@ -88,11 +90,43 @@ function getActiveRouteName(navigationState) {
     return route.routeName;
 }
 
+function getActiveColors(navigationState) {
+    if (!navigationState) {
+        return null;
+    }
+    const route = navigationState.routes[navigationState.index];
+    // dive into nested navigators
+    if (route.routes) {
+        return getActiveColors(route);
+    }
+
+    if (route.params && route.params.data) {
+        return route.params.data.colors;
+    }
+
+    return style.Gradient.default.colors;
+}
+
+function zIndexWorkaround(val) {
+    return Platform.select({
+        ios: { zIndex: val },
+        android: { elevation: val },
+    });
+}
+
 export default class App extends React.Component {
+    static router = RootStack.router;
+
     constructor(props) {
         super(props);
         this.state = {
+            colors: style.Gradient.Home.colors,
+            colorsLocation: style.Gradient.Home.locations,
+            prevColors: style.Gradient.Home.colors,
+            prevColorsLocation: style.Gradient.Home.locations,
             screen: 'Home',
+            tweener: new Animated.Value(0),
+            mustChangeColors: true,
         };
 
         this._onPressHome = this._onPressHome.bind(this);
@@ -116,6 +150,16 @@ export default class App extends React.Component {
         requestAnimationFrame(() => {
             this.navigator && this.navigator.dispatch(NavigationActions.navigate({ routeName: 'About' }));
         });
+    }
+
+    componentDidUpdate() {
+        const { tweener } = this.state;
+
+        if (this.state.mustChangeColors) {
+            Animated.timing(tweener, { toValue: 0 }).start();
+        } else {
+            Animated.timing(tweener, { toValue: 1 }).start();
+        }
     }
 
     render() {
@@ -150,37 +194,72 @@ export default class App extends React.Component {
             <SafeAreaView style={{ position: 'absolute', bottom: 0, alignSelf: 'center' }}>
                 <View style={[style.NavBar.bar, specificStyle]}>
                     <TouchableOpacity onPress={this._onPressHome} style={[style.NavBar.button, this.state.screen === 'Home' ? selected : {}]}>
-                        <SearchIcon/>
+                        <SearchIcon />
                     </TouchableOpacity>
                     <TouchableOpacity
                         disabled={true}
                         onPress={this._onPressCalendar}
                         style={[style.NavBar.button, { opacity: 0.5 }, this.state.screen === 'Calendar' ? selected : {}]}>
-                        <CalendarIcon/>
+                        <CalendarIcon />
                     </TouchableOpacity>
                     <TouchableOpacity
                         onPress={this._onPressSettings}
                         style={[style.NavBar.button, this.state.screen === 'About' ? selected : {}]}>
-                        <SettingsIcon/>
+                        <SettingsIcon />
                     </TouchableOpacity>
                 </View>
             </SafeAreaView>
         );
 
-        let gradientColors = style.Gradient.default.colors;
-        let gradientLocation = style.Gradient.default.location;
-
-        if (this.state.screen === 'Home' || this.state.screen === 'About') {
-            gradientColors = style.Gradient.Home.colors;
-            gradientLocation = style.Gradient.Home.location;
-        } else if (this.state.screen === 'WebBrowser') {
+        if (this.state.screen === 'WebBrowser') {
             navBar = null;
         }
 
+        const colorsOpacity = this.state.tweener.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [0, 1, 1],
+        });
+        const prevColorsOpacity = this.state.tweener.interpolate({
+            inputRange: [0, 0.5, 1],
+            outputRange: [1, 1, 0],
+        });
+
         return (
-            <LinearGradient colors={gradientColors} locations={gradientLocation} style={{ flex: 1, paddingTop: StatusBar.currentHeight }}>
-                <SafeAreaView style={{ flex: 1, position: 'relative' }}>
-                    <RootStack
+            <>
+                <Animated.View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        opacity: colorsOpacity,
+                        ...zIndexWorkaround(-1),
+                    }}>
+                    <LinearGradient colors={this.state.colors} locations={this.state.colorsLocation} style={{ flex: 1 }} />
+                </Animated.View>
+
+                <Animated.View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                        opacity: prevColorsOpacity,
+                        ...zIndexWorkaround(1),
+                    }}>
+                    <LinearGradient colors={this.state.prevColors} locations={this.state.prevColorsLocation} style={{ flex: 1 }} />
+                </Animated.View>
+
+                <SafeAreaView
+                    style={{
+                        paddingTop: StatusBar.currentHeight,
+                        flex: 1,
+                        position: 'relative',
+                        ...zIndexWorkaround(2),
+                    }}>
+                    <AppContainer
                         ref={(navigator) => (this.navigator = navigator)}
                         style={{ flex: 1 }}
                         onNavigationStateChange={(prevState, currentState) => {
@@ -188,13 +267,35 @@ export default class App extends React.Component {
                             const prevScreen = getActiveRouteName(prevState);
 
                             if (prevScreen !== currentScreen) {
-                                this.setState({ screen: currentScreen });
+                                if (currentScreen === 'Home' || currentScreen === 'About') {
+                                    return this.setState({
+                                        screen: currentScreen,
+                                        colors: this.state.mustChangeColors ? style.Gradient.Home.colors : this.state.colors,
+                                        colorsLocation: this.state.mustChangeColors ? style.Gradient.Home.locations : this.state.colorsLocation,
+                                        prevColors: this.state.mustChangeColors ? this.state.prevColors : style.Gradient.Home.colors,
+                                        prevColorsLocation: this.state.mustChangeColors
+                                            ? this.prevColorsLocation
+                                            : style.Gradient.Home.locations,
+                                        mustChangeColors: !this.state.mustChangeColors,
+                                        tweener: this.state.mustChangeColors ? new Animated.Value(0) : new Animated.Value(1),
+                                    });
+                                }
+
+                                this.setState({
+                                    screen: currentScreen,
+                                    colors: this.state.mustChangeColors ? getActiveColors(currentState) : this.state.colors,
+                                    colorsLocation: this.state.mustChangeColors ? [0, 1] : this.state.colorsLocation,
+                                    prevColors: this.state.mustChangeColors ? this.state.prevColors : getActiveColors(currentState),
+                                    prevColorsLocation: this.state.mustChangeColors ? this.prevColorsLocation : [0, 1],
+                                    mustChangeColors: !this.state.mustChangeColors,
+                                    tweener: this.state.mustChangeColors ? new Animated.Value(0) : new Animated.Value(1),
+                                });
                             }
                         }}
                     />
                     {navBar}
                 </SafeAreaView>
-            </LinearGradient>
+            </>
         );
     }
 }
